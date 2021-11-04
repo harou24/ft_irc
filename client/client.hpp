9b7c439fe20441812ff85b84c8ac05e206b49eb2
harou24 <haroutioun24@gmail.com>
2021-11-04 02:19:33 +0100

    get started

---
+++ client/client.hpp
@@ -0,0 +1,21 @@
+#ifndef CLIENT_HPP
+# define CLIENT_HPP
+
+#define MAX_BUFF_SIZE 256
+
+class Client {
+   private:
+        int                     fd;
+        struct sockaddr_storage remoteAddr;
+        char                    remoteIP[INET6_ADDRSTRLEN];
+        char                    dataBuffer[MAX_BUFF_SIZE];
+
+    public:
+        Client(void);
+        Client(int fd, struct sockaddr_storage remoteAddr, char *remoteIp);
+        ~Client(void);
+
+
+};
+
+#endif
---
+++ server/server.hpp
@@ -0,0 +1,31 @@
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# include <vector>
+
+# include "client.hpp"
+
+# define MAX_PENDING_CONNECTION 10
+
+class Server {
+
+    private:
+        std::vector<Client> clients;
+        fd_set              master;
+        fd_set              readsFds;
+        int                 listener;
+        int                 fdMax;
+
+    public:
+        Server(void);
+        ~Server(void);
+        
+        void    init(void);
+        void    run(void);
+        void    handleNewClient(void);
+        void    handleClientData(void);
+
+};
+
+
+#endif
