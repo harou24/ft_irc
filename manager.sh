12c7550f398838e976b97f48e5c5c2dc629ac7f4
harou24 <haroutioun24@gmail.com>
2021-11-05 20:13:21 +0100

    new makefile

--- manager.sh
+++
@@ -1,9 +0,0 @@
-if [ $# -eq 1 ] && [ "$1" == "build" ]; then
-    mkdir -p build && cmake -S . -B build && cmake --build build && cp build/irc_server.exe .;
-elif [ $# -eq 1 ] && [ "$1" == "clean" ]; then
-    git clean -d -f -X &&  rm -rf build && rm -f irc_server.exe;
-elif [ $# -eq 1 ] && [ "$1" == "test" ]; then
-     mkdir -p build && cmake -S . -B build && cmake --build build && cd build && ctest --verbose;
-else
-    echo "NO ARGS ERROR\n  Run -> bash manager build | test | cleean\n" ;
-fi
