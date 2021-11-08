f4a6f7179b542b790f42e9e951ebf3b656815172
harou24 <haroutioun24@gmail.com>
2021-11-08 01:01:36 +0100

    test catch 2 ok

---
+++ make.sh
@@ -0,0 +1,9 @@
+if [ $# -eq 1 ] && [ "$1" == "build" ]; then
+    mkdir -p build && cmake -S . -B build && cmake --build build;
+elif [ $# -eq 1 ] && [ "$1" == "clean" ]; then
+    git clean -d -f -X &&  rm -rf build;
+elif [ $# -eq 1 ] && [ "$1" == "test" ]; then
+     mkdir -p build && cmake -S . -B build && cmake --build build && cd build && ctest --verbose;
+else
+    echo "NO ARGS ERROR\n  Run -> bash manager build | test | cleean\n" ;
+fi
---
+++ tests/test.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch.hpp>
+
+#include <iostream>
+TEST_CASE("Class instantiates")
+{
+    std::cout << "TEST OK\n" << std::endl;
+}
