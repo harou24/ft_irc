b677c6a6fba923277a23377b99b010f72afd24f2
harou24 <haroutioun24@gmail.com>
2021-11-12 03:49:21 +0100

    save

--- tests/src/client.cpp
+++ tests/src/client.cpp
@@ -3,8 +3,6 @@
 #include <catch2/catch.hpp>
 
 #include <iostream>
-#include <string.h>
-#include <unistd.h>
 
 #include "client.hpp"
 #include "ostream_client.hpp"
@@ -13,15 +11,4 @@
 
 TEST_CASE("Send Data", "[CLIENT]")
 {
-    Client cl;
-    std::cout << cl;
-    int fdToSendTo = 4;
-    std::string dataToSend("HelloWorld\n");
-    cl.sendMsg(fdToSendTo, dataToSend);
-
-    char    buff[TEST_BUFF_SIZE];
-    memset(buff, 0, TEST_BUFF_SIZE);
-    read(fdToSendTo, buff, TEST_BUFF_SIZE);
-    std::string dataRead(buff);
-    REQUIRE(dataToSend == dataRead);
 }
